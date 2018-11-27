391483f7d0639a332fb06cd32068b0d3ef7d69f8
ArunaKumariM <43091704+ArunaKumariM@users.noreply.github.com>
2018-11-27 19:05:51 +0530

    Add files via upload

---
+++ assignment_RmcrdpluginPackage not found.r
@@ -0,0 +1,24 @@
+library(RcmdrPlugin.IPSUR)
+install.packages("RmcdrPlugin.IPSUR")
+## package cannot be found
+
+
+> install.packages("RmcdrPlugin.IPSUR")
+Installing package into 'C:/Users/hp/Documents/R/win-library/3.5'
+(as 'lib' is unspecified)
+Warning in install.packages :
+  package 'RmcdrPlugin.IPSUR' is not available (for R version 3.5.1)
+Warning in install.packages :
+  unable to access index for repository http://www.stats.ox.ac.uk/pub/RWin/bin/windows/contrib/3.5:
+  cannot open URL 'http://www.stats.ox.ac.uk/pub/RWin/bin/windows/contrib/3.5/PACKAGES'
+> library(RcmdrPlugin.IPSUR)
+Error in library(RcmdrPlugin.IPSUR) : 
+  there is no package called 'RcmdrPlugin.IPSUR'
+> install.packages("RmcdrPlugin.IPSUR")
+Installing package into 'C:/Users/hp/Documents/R/win-library/3.5'
+(as 'lib' is unspecified)
+Warning in install.packages :
+  package 'RmcdrPlugin.IPSUR' is not available (for R version 3.5.1)
+Warning in install.packages :
+  unable to access index for repository http://www.stats.ox.ac.uk/pub/RWin/bin/windows/contrib/3.5:
+  cannot open URL 'http://www.stats.ox.ac.uk/pub/RWin/bin/windows/contrib/3.5/PACKAGES'
